df9ab5141c8bceaaa8999a027a70c018b8a49ea1
Athanasios (Thanos) Vourvopoulos <8280871+athanoid@users.noreply.github.com>
2021-07-21 19:52:52 +0100

    first commit

---
+++ Assets/Scripts/ReceiveHandData.cs
@@ -0,0 +1,148 @@
+﻿/*
+    @author: Thanos
+    -----------------------
+    UDP-Receive (from python client)
+    -----------------------
+    // [url]https://google.github.io/mediapipe/solutions/hands[/url]
+   
+    // > receive
+    // 127.0.0.1 : 5005
+   
+    // send
+    // nc -u 127.0.0.1 5005
+ 
+*/
+using UnityEngine;
+using System.Collections;
+
+using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Collections.Generic;
+
+public class ReceiveHandData : MonoBehaviour {
+
+    // receiving Thread
+    Thread receiveThread;
+
+    // udpclient object
+    UdpClient client;
+
+    // public string IP = "127.0.0.1"; default local
+    public int port; // define > init
+
+    //[url]https://google.github.io/mediapipe/images/mobile/hand_landmarks.png[/url]
+    //IDictionary<string, float> handLandmarks = new Dictionary<string, float>();
+
+    public GameObject  WRIST, INDEX_FINGER_TIP;
+    Vector3 p0pos, p8pos;
+    public float amp = 1;
+
+    // start
+    public void Start()
+    {
+        init();
+    }
+
+    public void Update() {
+        WRIST.transform.position = -p0pos;
+        INDEX_FINGER_TIP.transform.position = -p8pos;
+    }
+
+    // init
+    private void init()
+    {
+        print("UDPSend.init()");
+
+        // define port
+        port = 5005;
+
+        // status
+        print("Sending to 127.0.0.1 : " + port);
+        print("Test-Sending to this Port: nc -u 127.0.0.1  " + port + "");
+
+        // Create a receive-data thread
+        receiveThread = new Thread(
+            new ThreadStart(ReceiveData));
+        receiveThread.IsBackground = true;
+        receiveThread.Start();
+
+    }
+
+    // receive thread
+    private void ReceiveData()
+    {
+
+        client = new UdpClient(port);
+        while (true)
+        {
+
+            try
+            {
+                // Receive bytes 
+                IPEndPoint anyIP = new IPEndPoint(IPAddress.Any, 0);
+                byte[] data = client.Receive(ref anyIP);
+
+                // convert bytes to float array
+                float [] newdata = ConvertByteToFloat(data);
+
+                //foreach (float coord in newdata){print(coord);}
+
+                //foreach (KeyValuePair <string, float> mark in handLandmarks)
+                //  Debug.LogFormat("Key: {0}, Value: {1}", mark.Key, mark.Value);
+                // for (int i = 0; i < newdata.Length; i++)
+                // {
+                //print(newdata[1]+ newdata[2]+ newdata[3]);
+                //Instantiate(handPoint, new Vector3(newdata[1], newdata[2], newdata[3]), Quaternion.identity);
+                //    Rigidbody clone;
+                //    clone = Instantiate(handPoint, transform.position, transform.rotation);
+                if (newdata[0] == 0f) {
+
+                    p0pos = new Vector3(newdata[1], newdata[2], newdata[3]) * amp;
+                    //print(wristpos);
+                    
+                }
+                if (newdata[0] == 8f)
+                {
+
+                    p8pos = new Vector3(newdata[1], newdata[2], newdata[3]) * amp;
+                    //print(wristpos);
+
+                }
+
+                //  }
+            }
+            catch (Exception err)
+            {
+                print(err.ToString());
+            }
+        }
+    }
+
+    public static float[] ConvertByteToFloat(byte[] array)
+    {
+        float[] floatArr = new float[array.Length / 4];
+        for (int i = 0; i < floatArr.Length; i++)
+        {
+            if (BitConverter.IsLittleEndian)
+            {
+                Array.Reverse(array, i * 4, 4);
+            }
+            floatArr[i] = BitConverter.ToSingle(array, i * 4);
+        }
+        return floatArr;
+    }
+
+
+    void OnDisable()
+    {
+        if (receiveThread != null)
+            receiveThread.Abort();
+
+        client.Close();
+    }
+
+
+}
---
+++ Python_Client/openCVhandtrack.py
@@ -0,0 +1,80 @@
+# https://github.com/Ankit0822/Hand-Tracking-using-opencv-python-and-mediapipe
+# pip install opencv-python
+# pip install mediapipe
+
+import cv2
+import mediapipe as mp
+import time
+import socket
+import numpy as np
+from struct import *
+
+UDP_IP = "127.0.0.1"
+UDP_PORT = 5005
+
+print("UDP target IP: %s" % UDP_IP)
+print("UDP target port: %s" % UDP_PORT)
+
+sock = socket.socket(socket.AF_INET, # Internet
+                     socket.SOCK_DGRAM) # UDP
+
+# Create Video object
+cap = cv2.VideoCapture(0)
+# Formality we have to write before start
+# using this model
+mpHands = mp.solutions.hands
+# Creating an object from class Hands
+hands = mpHands.Hands()
+# creating an object to draw hand landmarks
+mpDraw = mp.solutions.drawing_utils
+# Previous time for frame rate
+pTime = 0
+# Current time for frame rate
+cTime = 0
+
+while True:
+    # Getting our Frame
+    success, img = cap.read()
+    # Convert image into RGB
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    # Calling the hands object to the getting results
+    results = hands.process(imgRGB)
+    #print(results.multi_hand_landmarks)
+
+    # Checking something is detected or not
+    if results.multi_hand_landmarks :
+        # Extracting the multiple hands
+        # Go through each hand
+        for handLms in results.multi_hand_landmarks:
+            # Getting id(index number) and landmark of each hand
+            for id, lm in enumerate(handLms.landmark):
+                print(id,lm)
+                #print(type(id))
+                #bytes object containing the values packed according to the format
+                coord_struct = pack('!ffff', float(id), lm.x, lm.y, lm.z)
+                #print(coord_struct)
+                #send via UDP
+                sock.sendto(coord_struct, (UDP_IP, UDP_PORT))
+                # Height, width and channel of image
+                h, w, c = img.shape
+                # X and Y coordinate
+                # their values in decimal so
+                # we have to convert into pixel
+                cx, cy = int(lm.x*w),int(lm.y*h)
+                #print(id, cx, cy)
+                #if id ==4:
+                cv2.circle(img, (cx, cy), 7, (255,0,255), cv2.FILLED)
+            # Draw the landmarks and line of the each hands
+            mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
+    # Getting the current time
+    cTime = time.time()
+    # Getting frame per second
+    fps = 1/(cTime-pTime)
+    # Previous time become current time
+    pTime = cTime
+    # Labeling the Frame rate
+    cv2.putText(img,str(int(fps)),(10,70),cv2.FONT_HERSHEY_PLAIN,3,
+                            (255,0,255),3)
+
+    cv2.imshow("image",img) # Show the frame
+    cv2.waitKey(1) # Wait for 1 millisecond
